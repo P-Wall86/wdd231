b50ccb537bcab35f7d61b91bb81d8c903a3f36d5
P-Wall86 <sisterwall@gmail.com>
2025-03-26 14:24:50 -0300

    Form so far

---
+++ chamber/scripts/form.js
@@ -0,0 +1,11 @@
+document.querySelector("#orgtitle").addEventListener("input", function () {
+    const value = this.value;
+    if (value.length > 16) {
+        this.setCustomValidity("Maximum 16 characters allowed.");
+    } else {
+        this.setCustomValidity("");
+    }
+});
+document.querySelector("#orgtitle").addEventListener("invalid", function () {
+    this.setCustomValidity("Org title must be at least 7 letters, hyphens, or spaces (no numbers/symbols).");
+});
